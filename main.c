09ee305a4bb4fdadd3b50b141f5e7dcb26779535
Sokol123456 <nikolay_yashenov@mail.ru>
2020-10-26 19:11:16 +0300

    fix preproccess

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdbool.h";
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int randomInt(int min, int max) // Для генерации рандомного числа
 { 
